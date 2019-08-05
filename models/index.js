41ba5b31b2c7aff4a85e0852ba633d0395dd7560
John Florez <comunicaciones@appsmedellin.com>
2019-08-05 00:17:50 -0500

    Backend Perfiles
    Perfiles backend

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const {Profile} = require('./ProfileModel')
+
+const DB_URL_MONGO = 'mongodb+srv://faber:faber123@cluster0-uqdxp.mongodb.net/test?retryWrites=true&w=majority'
+
+//Cadena de conexión y se manda a llamar los modelos
+mongoose.connect(DB_URL_MONGO,{ useNewUrlParser: true },(err)=>{
+  if (!err) console.log('Conexión exitosa');
+});
+
+module.exports = {
+  Profile
+}
